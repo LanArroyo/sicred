9625c336c71815820288b3fa20f07f8b50b643d2
DESKTOP-LGSGFS3\colbach <lan.arroy@gmail.com>
2017-08-09 08:43:59 -0500

    Clase para destruir la sesion

---
+++ module/Sicred/src/Model/logout.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: colbach
+ * Date: 08/08/2017
+ * Time: 12:42
+ */
+namespace Sicred\Model;
+
+use Sicred\Model;
+class logout{
+
+    public function closeSession(){
+        session_start();
+        if(isset($_SESSION['pendmat'])){
+            $serch = new Model\serchStudent();
+            $serch->updateStudent($_SESSION['pendmat']);
+            session_destroy();
+        }else{
+            session_destroy();
+        }
+    }
+
+
+}
