97bbad6cda1c7dd6e0cf8d0e957cfdbc541531ae
DESKTOP-LGSGFS3\colbach <lan.arroy@gmail.com>
2017-08-14 10:36:43 -0500

    Se creó la clase agregarCredencial para llevar un registro de las credenciales

--- module/Sicred/src/Model/login.php
+++ module/Sicred/src/Model/login.php
@@ -9,50 +9,45 @@
 // 2170104
 namespace Sicred\Model;
 
+use Doctrine\DBAL\Driver\PDOException;
+
 class login{
     private $db;
 
     public function startSession($user, $pwd){
         self::Connection();
-        $sql = "SELECT NAME FROM PRIMER_INGRESO.SYS_USER WHERE MATRICULA='".$user."'"; //cambiar
-        $query = $this->db->query($sql);
-        $row = $query->fetch();
-
-        //verificamos si se encontro el usuario
-        if(isset($row)){
-            $sql = "SELECT NAME FROM USRCB WHERE PWD ='".$pwd."'";
+        $sql = "SELECT PASSWORD, CT_USER FROM PRIMER_INGRESO.SYS_USER WHERE MATRICULA='".$user."'"; //cambiar
+        try{
             $query = $this->db->query($sql);
             $row = $query->fetch();
-            //verificamos que la contraseña sea la carrecta
+
+            //verificamos si se encontro el usuario
             if(isset($row)){
-                session_start();
-                $_SESSION['aprobmat'] = null;
-                $_SESSION['pendmat'] = null;
-                $_SESSION['user_active'] = $row[0];
-                //regresamos 1 de exito
-                return 1;
+                if($row[0] == $pwd){
+                    session_start();
+                    $_SESSION['aprobmat'] = null;
+                    $_SESSION['pendmat'] = null;
+                    $_SESSION['user'] = $user;
+                    $_SESSION['typeuser'] = $row[1];
+                    return 1;
+                }else{
+                    //regresamos -1 el usuario se equivoco de contraseña
+                    return -1;
+                }
             }else{
-                //-2 el usuario se equivoco de contraseña
+                //no existe el usuario
                 return -2;
             }
-        }else{
-            //regresamos -1 el usuario no existe
-            return -1;
-        }
-        if($user == '2170104' and $pwd == 'admin'){
-            session_start();
-            $_SESSION['aprobmat'] = null;
-            $_SESSION['pendmat'] = null;
-            $_SESSION['user_active'] = $user;
-            return 1;
-        }
 
+        }catch (PDOException $e){
+
+        }
     }
 
     private function Connection(){
         $conn = null;
         try{
-            $conn = new \PDO("oci:host=129.144.51.99;dbname=NUBEBDU;port=1524", "ADMPORTAL","CBUSRADMORA11");
+            $conn = new \PDO("oci:host=10.100.30.76;dbname=ORACRED;port=1523", "ADMPORTAL","CBUSRADMORA11");
             $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         }catch (PDOException $e){ ?>
             <script>console.log("ERROR: "' . <?php $e->getMessage()?> .') </script>
